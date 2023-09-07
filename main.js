0cdc49bafc71e497294fe5ac93dbcd20ca6b3e49
Prasun2013 <140720867+Prasun2013@users.noreply.github.com>
2023-09-07 09:38:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+console.log(first_name);
+console.log(last_name);
+
+first_name.concat(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+console.log(a);
+console.log(b);
+
+var add = a + b;
+console.log(add);
